3a56208fa3b46efa84fa615a53fd587e349fdd37
Lars Görisch <lars-goerisch@web.de>
2022-04-10 13:32:21 +0200

    init

--- dist/A.js
+++
@@ -1,7 +0,0 @@
-export class A {
-    hi(name) {
-        const test = "test";
-        return "hello " + name;
-    }
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQS5qcyIsInNvdXJjZVJvb3QiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvc291cmNlcy8iLCJzb3VyY2VzIjpbIkEudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxPQUFPLENBQUM7SUFFSCxFQUFFLENBQUMsSUFBYTtRQUNuQixNQUFNLElBQUksR0FBRyxNQUFNLENBQUM7UUFDcEIsT0FBTyxRQUFRLEdBQUcsSUFBSSxDQUFDO0lBQzNCLENBQUM7Q0FDSiJ9
